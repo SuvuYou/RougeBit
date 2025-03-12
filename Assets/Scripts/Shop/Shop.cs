208a5af2ba2f703ab1747c1494f5a20d3cf93376
SuvuYou <nazarsuvyliak1@gmail.com>
2025-03-12 19:50:04 +0200

    setup card prefab and rarity

--- Assets/Scripts/Shop/Shop.cs
+++ Assets/Scripts/Shop/Shop.cs
@@ -2,14 +2,21 @@ using UnityEngine;
 
 class Shop : MonoBehaviour
 {
+    [SerializeField] private SerializableDictionary<Rarity, CardRarityConfig> _cardRarityConfigs;
     [SerializeField] private Buyer _buyer = null;
 
     private void _displayItems()
     {
         if (_buyer.TrySelectRandomItemToBuy(out UpgradablesBundleController item))
         {
-            // Debug.Log(item.GetNextLevelInfo().Title);
-            _buyer.Purchace(item);
+            // item.GetNextLevelInfo().Title;
+            // item.GetNextLevelInfo().IconSprite;
+            // item.GetNextLevelInfo().TitleSprite;
+            // item.GetNextLevelInfo().UpgradeRarity;
+
+            // _cardRarityConfigs[item.GetNextLevelInfo().UpgradeRarity];
+
+            // _buyer.Purchace(item);
         }
     }
 
