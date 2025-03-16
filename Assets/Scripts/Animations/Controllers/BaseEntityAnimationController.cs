233be68338819a3294ab51c4c641cbbbadd16a77
SuvuYou <nazarsuvyliak1@gmail.com>
2025-03-16 22:28:18 +0200

    face sprites correctly

--- Assets/Scripts/Animations/Controllers/BaseEntityAnimationController.cs
+++ Assets/Scripts/Animations/Controllers/BaseEntityAnimationController.cs
@@ -6,11 +6,13 @@ public class BaseEntityAnimationController : BaseAnimationController
     [SerializeField] protected CharacterMovement _characterMovement;
     [SerializeField] protected SpriteRenderer _spriteRenderer;
 
+    [SerializeField] protected bool _isSpriteRightBased = true;
+
     protected bool _isFaceDown => _characterMovement.LastNonZeroDirection.y < 0;
     protected bool _isFaceLeft => _characterMovement.LastNonZeroDirection.x < 0;
 
     protected virtual void Update()
     {
-        _spriteRenderer.flipX = !_isFaceLeft;
+        _spriteRenderer.flipX = _isSpriteRightBased && _isFaceLeft || !_isSpriteRightBased && !_isFaceLeft;
     }
 }
