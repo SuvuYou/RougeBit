3a8188216e7a6810d915028ae66a25bf90587145
SuvuYou <nazarsuvyliak1@gmail.com>
2025-03-22 00:13:22 +0200

    clean code

--- Assets/Scripts/Attacks/RocketLaunchAttack.cs
+++ Assets/Scripts/Attacks/RocketLaunchAttack.cs
@@ -52,7 +52,6 @@ class RocketLaunchAttack : BaseAttack
 
     protected override void _handleAim(Action cancelAim, Action earlyFinishAttack)
     {
-        Debug.Log(_aimTimer.IsRunning);
         if (_attackerMovement.Velocity.magnitude > _stats.MovementVelocityToCancelAttackThreshold) cancelAim();
         _updateAttackState();
     }
